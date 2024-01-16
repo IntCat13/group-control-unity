4d053d454a6a7353eaacde20d3f64e2bee74be0a
Waulesi <85766091+IntCat13@users.noreply.github.com>
2024-01-16 23:54:52 +0100

    Asdef references fix
    Gitignore file where changed to ignore asdef meta files

--- U.GroupControl/Assets/_GroupControl/Core/AssetsLoadingSystem/AssetLoader.cs
+++ U.GroupControl/Assets/_GroupControl/Core/AssetsLoadingSystem/AssetLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -16,16 +17,24 @@ namespace _GroupControl.Core.AssetsLoadingSystem
 
         public async Task<GameObject[]> LoadCharactersAsync(IAssetsConfig config, int characterCount = 1)
         {
-            GameObject characterPrefab = await config.CharacterPrefabReference.LoadAssetAsync<GameObject>().Task;
-            GameObject[] characters = new GameObject[characterCount];
-            for (int i = 0; i < characterCount; i++)
-            {
-                Vector3 randomPosition = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
-                characters[i] = Instantiate(characterPrefab, randomPosition, Quaternion.identity);
-            }
+            try
+                {
+                    GameObject characterPrefab = await config.CharacterPrefabReference.LoadAssetAsync<GameObject>().Task;
+                    GameObject[] characters = new GameObject[characterCount];
+                    for (int i = 0; i < characterCount; i++)
+                    {
+                        Vector3 randomPosition = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+                        characters[i] = Instantiate(characterPrefab, randomPosition, Quaternion.identity);
+                    }
             
-            config.CharacterPrefabReference.ReleaseAsset();
-            return characters;
+                    config.CharacterPrefabReference.ReleaseAsset();
+                    return characters;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Error loading characters: {e.Message}\nStack trace: {e.StackTrace}");
+                    throw; // Rethrow the exception after logging for further analysis.
+                }
         }
     }
 }
